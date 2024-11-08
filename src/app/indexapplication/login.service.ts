ae7200d4fdb27f68dc308b0442f87966d1b5c3ee
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-08 18:47:30 +0100

    Update login.service.ts d

--- src/app/indexapplication/login.service.ts
+++ src/app/indexapplication/login.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';  // Chemin correct vers l'environnement
-import { User } from './userconnexion';  // Assurez-vous que le chemin du modèle est correct
+
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, map } from "rxjs";
+import { environment } from "../../environment";
+import { User } from "./userconnexion";
 
 @Injectable({
-  providedIn: 'root',  // Ce décorateur permet au service d'être injectable dans toute l'application
+  providedIn: "root",
 })
-export class ConnexionService {  // Vérifiez le nom du service ici
-  constructor(private http: HttpClient) {}
+export class connexionService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  login(connexion: any): Observable<User> {
-    return this.http.post<User>(`${environment.apiUrl}/auth/login`, connexion).pipe(
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem("currentUser") || '{}')
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(connexion: any) {
+    return this.http.post<any>(`${environment.apiUrl}/auth/login`, connexion).pipe(
       map((userconnexion) => {
         if (userconnexion && userconnexion.token) {
-          localStorage.setItem('User', JSON.stringify(userconnexion));
-          localStorage.setItem('Role', userconnexion.profile);
-          localStorage.setItem('Email', userconnexion.email);
-          localStorage.setItem('Token', userconnexion.token);
+          localStorage.setItem("User", JSON.stringify(userconnexion));
+          localStorage.setItem("Role", userconnexion.profil);
+          localStorage.setItem("Email", userconnexion.email);
+          localStorage.setItem("Id", userconnexion.id);
+          localStorage.setItem("Token", userconnexion.token);
+
+          this.currentUserSubject.next(userconnexion);
           return userconnexion;
         }
-        return null;
       })
     );
   }
