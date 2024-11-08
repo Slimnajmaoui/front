f32c1e29312f42c5975b9e06fb41806f900748c7
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-08 23:59:09 +0100

    Update index.js s4

--- rest-crud/index.js
+++ rest-crud/index.js
@@ -37,9 +37,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(cors({
-  origin: 'https://20.55.80.124:8090'
-}));
+app.use(cors());
   
 /* Static directory path */
 app.use(express.static(path.join(__dirname, 'dist/angular-mean-crud-tutorial')));
