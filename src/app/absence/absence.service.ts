49c4dd05f2d1b6f82afa86954615a35c4bdf1dc6
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-10 20:06:07 +0100

    Update absence.service.ts hh

--- src/app/absence/absence.service.ts
+++ src/app/absence/absence.service.ts
@@ -12,7 +12,7 @@ import { Absence } from './absence';
 })
 export class AbsenceService {
     
-  private apiURL = "http://localhost:8081/api";
+  private apiURL = "http://20.55.80.124:8090/api";
       
   /*------------------------------------------
   --------------------------------------------
