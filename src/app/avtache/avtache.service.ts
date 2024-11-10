8c91d65b58beef10b2511918085ec6664d04782c
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-10 20:09:29 +0100

    Update avtache.service.ts dks

--- src/app/avtache/avtache.service.ts
+++ src/app/avtache/avtache.service.ts
@@ -12,7 +12,7 @@ import { Avtache } from './avtache';
 })
 export class AvtacheService {
     
-  private apiURL = "http://localhost:8081/api";
+ private apiURL = "http://20.55.80.124:8090/api";
       
   /*------------------------------------------
   --------------------------------------------
