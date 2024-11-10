b4204132c7befaf465bec8526a3a1a60627ac889
Slim Nejmaoui <85970006+Slimnajmaoui@users.noreply.github.com>
2024-11-10 20:13:19 +0100

    Update profil.service.ts hh

--- src/app/profil/profil.service.ts
+++ src/app/profil/profil.service.ts
@@ -12,7 +12,7 @@ import { Profil } from './profil';
 })
 export class ProfilService {
     
-  private apiURL = "http://localhost:8081/api";
+  private apiURL = "http://20.55.80.124:8090/api";
       
   /*------------------------------------------
   --------------------------------------------
